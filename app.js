407377d29927c656f6f42a090e7735079a6223b2
SiddhantZawar06 <siddhantzawar0@gmail.com>
2026-02-17 18:27:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+let currentIndex = 0;
+let images = document.querySelectorAll('.gallery img');
+let lightbox = document.getElementById('lightbox');
+let lightboxImg = document.getElementById('lightbox-img');
+
+function openLightbox(index){
+    currentIndex = index;
+    lightbox.style.display = "flex";
+    lightboxImg.src = images[currentIndex].src;
+}
+
+function closeLightbox(){
+    lightbox.style.display = "none";
+}
+
+function nextImage(){
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+}
+
+function prevImage(){
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+}
+
+/* Filter Images */
+function filterImages(category){
+    images.forEach(img => {
+        if(category === "all"){
+            img.style.display = "block";
+        }
+        else if(img.classList.contains(category)){
+            img.style.display = "block";
+        }
+        else{
+            img.style.display = "none";
+        }
+    });
+}
